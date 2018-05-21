e9f0955e6dc546915847b6ceb3aeba72ae65f337
Alex Hunter <alexander.hunter.80@gmail.com>
2018-05-21 12:15:17 -0700

    mapsTest has working marker placement and lat-lng readout

--- testing/mapsTest/apps/firstTest/urls.py
+++ testing/mapsTest/apps/firstTest/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views #pylint: disable = E0402
 
 urlpatterns = [
+    path('embed', views.embed),
     path('', views.index)
 ]
--- testing/mapsTest/apps/firstTest/views.py
+++ testing/mapsTest/apps/firstTest/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import redirect, render, HttpResponse
 def index(request):
 
     return render(request, 'firstTest/index.html')
+
+
+
+def embed(request):
+
+    return render(request, 'firstTest/embedded.html')
