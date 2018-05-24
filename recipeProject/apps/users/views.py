d6f0c3046aca5b9a671287fde2cc65b799fa8f54
tpups <jghtch@gmail.com>
2018-05-24 10:48:34 -0700

    commented out broken code in progress. Google works.

--- recipeProject/apps/users/views.py
+++ recipeProject/apps/users/views.py
@@ -3,4 +3,11 @@ from .models import *
 from django.contrib import messages
 
 def index(request):
+    # @receiver(post_save, sender=User)
+    # def create_new_user(sender, instance, created, **kwargs):
+    # if created:
+    #     return redirect("/newuser")
     return render(request, "users/home_page.html")
+
+def newuser(request):
+    return render()
